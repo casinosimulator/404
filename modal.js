dce7c0b7e5df514232c1ec153cfd4f7229fb8565
casinosimulator <148355972+casinosimulator@users.noreply.github.com>
2024-01-03 23:31:15 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -8,8 +8,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Lidar com o clique no botão "Jogar Dinheiro Real"
     jogarDinheiroRealButton.addEventListener("click", function() {
-        window.location.href = "https://gml-grp.com/C.ashx?btag=a_34857b_1821c_&affid=11229&siteid=34857&adid=620&c=cassino";
-
+        window.location.href = "https://gml-grp.com/C.ashx?btag=a_34857b_1821c_&affid=11229&siteid=34857&adid=620&c=cassino"; // Redireciona para o Google
+    });
+    
     // Lidar com o clique no botão "Dinheiro Virtual"
     dinheiroVirtualButton.addEventListener("click", function() {
         modal.style.display = "none"; // Fecha o modal
