51d367db621d54b80757ec588b07db40bc317636
casinosimulator <148355972+casinosimulator@users.noreply.github.com>
2024-01-09 10:29:42 -0300

    Create modal2.js

---
+++ modal2.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("modal");
+    var jogarDinheiroRealButton = document.getElementById("jogar-dinheiro-real");
+    var dinheiroVirtualButton = document.getElementById("dinheiro-virtual");
+
+    // Exibir o modal assim que a página carregar
+    modal.style.display = "block";
+
+    // Lidar com o clique no botão "Jogar Dinheiro Real"
+    jogarDinheiroRealButton.addEventListener("click", function abrirLinks() {
+      // Abre o primeiro link em uma nova aba
+  window.open("https://www.google.com", "_blank");
+
+  // Abre o segundo link na mesma aba
+  window.location = "https://www.facebook.com";
+});
+
+    // Lidar com o clique no botão "Dinheiro Virtual"
+    dinheiroVirtualButton.addEventListener("click", function() {
+        modal.style.display = "none"; // Fecha o modal
+    });
+});
